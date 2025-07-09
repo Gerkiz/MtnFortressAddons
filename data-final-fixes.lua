49958eac4c4567673dd2d2a61145ca9565805e13
Gerkiz <fs@nvfs.se>
2025-07-09 22:51:05 +0200

    Disable inserter custom vectors

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,7 +4,7 @@ require 'prototypes.decoratives'
 require 'prototypes.items'
 
 for _, inserter in pairs(data.raw.inserter) do
-	inserter.allow_custom_vectors = true
+	inserter.allow_custom_vectors = false
 end
 
 return Public
